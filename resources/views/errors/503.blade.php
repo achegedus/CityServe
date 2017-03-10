baca8d8841b7df5dbaabb5931cbe27642f027b99
Adam Hegedus <adam@heged.us>
2017-03-09 22:02:56 -0500

    Added 503 template

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -14,7 +14,8 @@
                 margin: 0;
                 padding: 0;
                 width: 100%;
-                color: #B0BEC5;
+                color: #FFFFFF;
+                background-color: #231c1a;
                 display: table;
                 font-weight: 100;
                 font-family: 'Lato', sans-serif;
@@ -35,12 +36,19 @@
                 font-size: 72px;
                 margin-bottom: 40px;
             }
+
+            p {
+                font-size: 26px;
+            }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">CITYSERVE 2017</div>
+                <div>
+                    <p>Check back on Monday, March 13 to register your projects.</p>
+                </div>
             </div>
         </div>
     </body>
