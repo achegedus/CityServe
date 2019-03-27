e664d3132cc0286924790143abed3e9fd670a82a
Adam Hegedus <adam.hegedus@energycap.com>
2019-03-27 02:24:58 -0400

    Updated serve date.

--- resources/assets/js/components/AboutUs.vue
+++ resources/assets/js/components/AboutUs.vue
@@ -7,10 +7,10 @@
             <p class="lead">CityServe is a partnership of several local churches to bless our local community. It's an opportunity for the church to be the church through practical acts of kindness. We're looking forward to completing more than 150 projects using 1500 volunteers on April 27 & 28! All types of projects will be requested - painting, yard work, cleaning and organizing, baking, home repair/construction, visiting with folks, hosting events and more. </p>
             <div style="text-align: center; margin: 40px;">
                 <router-link :to="{ name: 'request-project'}" class="btn btn-u-lg btn-primary rounded-2x" style="width: 300px;">Request a project?</router-link>
-                <a href="#" class="btn btn-u-lg btn-primary rounded-2x" style="width: 300px;" disabled>Volunteer to Serve (Opens 3/29)</a>
+                <a href="#" class="btn btn-u-lg btn-primary rounded-2x" style="width: 300px;" disabled>Volunteer to Serve (Opens 3/30)</a>
             </div>
             <!--<p class="lead">Please <router-link :to="{ name: 'serve' }">register to serve on a project between March 23 and April 15.</router-link> Volunteers can register as an individual or group.</p>-->
-            <p class="lead">Registrations to serve on a project will open on March 29. Volunteers can register as an individual or group.</p>
+            <p class="lead">Registrations to serve on a project will open on March 30. Volunteers can register as an individual or group.</p>
 
         </div>
     </section>
